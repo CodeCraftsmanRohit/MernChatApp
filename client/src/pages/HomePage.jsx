2098663594e1346d1990ef15b4dcf14c6ae54dde
Rohit Kumar <rohitkumar.ak51@gmail.com>
2026-01-28 20:23:14 +0530

    Home page UI started

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,32 @@
+import React,{useState} from 'react'
+import Sidebar from '../components/SIdebar'
+import ChatContainer from '../components/ChatContainer'
+import RightSideBar from '../components/RightSideBar'
+
+const HomePage = () => {
+const [selectedUser, setSelectedUser] = useState(null);
+
+  return (
+    <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
+  <div
+    className={
+      `backdrop-blur-xl border-2 border-gray-600 rounded-2xl
+       overflow-hidden h-[100%] grid grid-cols-1 relative
+       ${selectedUser
+         ? 'grid-cols-[1fr_1fr_1fr] xl:grid-cols-[1fr_2fr_1fr]'
+         : 'md:grid-cols-2'}`
+    }
+  >
+
+
+    <Sidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+    <ChatContainer  selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+    <RightSideBar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+
+</div>
+
+    </div>
+  )
+}
+
+export default HomePage

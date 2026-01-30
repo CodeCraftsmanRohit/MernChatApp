8175175f267fc033f350ff3dbb09032f1d65e702
Rohit Kumar <rohitkumar.ak51@gmail.com>
2026-01-30 14:32:25 +0530

    bgImage Import issue solved 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   const { authUser } = useContext(AuthContext);
 
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')] bg-cover min-h-screen">
+    <div className="bg-[url('/bgImage.svg')] bg-cover min-h-screen">
       <Routes>
         <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />} />
         <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
